7656850c65c33eb07ca800beeead27aecdd24895
Anastasia Silyutina <ASilyutina@slb.com>
2023-01-15 18:07:14 +0300

    task51

---
+++ 5.1_delete_world.py
@@ -0,0 +1,11 @@
+# 51. Напишите программу, удаляющую из текста все слова содержащие "абв".
+
+text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+def del_some_words(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+text = del_some_words(text)
+print(text)
